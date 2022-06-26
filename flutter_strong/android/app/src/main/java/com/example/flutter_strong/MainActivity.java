967e1136dc9d014fc401d862a8dedae7f41a7370
AC_huang <huangzhifei2009@126.com>
2022-06-26 10:00:06 +0800

    通过命令 flutter create -i objc -a java xx 切换项目初始化为oc、java项目，否则默认是 swift、kotlin项目

---
+++ flutter_strong/android/app/src/main/java/com/example/flutter_strong/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_strong;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- flutter_strong/android/app/src/main/kotlin/com/example/flutter_strong/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_strong
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
