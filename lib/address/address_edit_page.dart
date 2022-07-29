2c1e344def2ba6d38aa7e8444c3dcacbe7d237e5
AC_huang <huangzhifei2009@126.com>
2022-07-29 12:09:24 +0800

    完成 地址 辅助页面

--- lib/address/address_edit_page.dart
+++ lib/address/address_edit_page.dart
@@ -1,15 +1,135 @@
+import 'package:city_pickers/city_pickers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_strong/services/events_bus.dart';
+import 'package:flutter_strong/services/screen_adaper.dart';
+import 'package:flutter_strong/services/user_services.dart';
+import 'package:flutter_strong/uikit/fs_button.dart';
+import 'package:flutter_strong/uikit/fs_text.dart';
 
 class AddressEditPage extends StatefulWidget {
-  const AddressEditPage({Key? key}) : super(key: key);
+  final Map arguments;
+
+  const AddressEditPage({Key? key, required this.arguments}) : super(key: key);
 
   @override
   State<AddressEditPage> createState() => _AddressEditPageState();
 }
 
 class _AddressEditPageState extends State<AddressEditPage> {
+
+  String area = "";
+
+  // 初始化的时候给编辑页面赋值
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    nameController.text = widget.arguments["name"];
+    phoneController.text = widget.arguments["phone"];
+    addressController.text = widget.arguments["address"];
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+
+    eventBus.fire(AddressEvent("编辑成功..."));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    ScreenAdaper.init(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("修改收货地址"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            const SizedBox(height: 20,),
+            FSText(
+              controller: nameController,
+              text: "收货人姓名",
+              onChanged: (value) {
+                nameController.text = value;
+              },
+            ),
+            const SizedBox(height: 10,),
+            FSText(
+              controller: phoneController,
+              text: "收货人电话",
+              onChanged: (value) {
+                phoneController.text = value;
+              },
+            ),
+
+            const SizedBox(height: 10,),
+            // 地址
+            Container(
+              padding: const EdgeInsets.only(left: 5),
+              height: ScreenAdaper.height(68),
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(width: 1, color: Colors.black12),
+                )
+              ),
+              child: InkWell(
+                child: Row(
+                  children: <Widget>[
+                    const Icon(Icons.add_location),
+                    area.isNotEmpty ? Text(area) : const Text("省/市/区", style: TextStyle(color: Colors.black54),)
+                  ],
+                ),
+                onTap: () async {
+                  Result? result = await CityPickers.showCityPicker(
+                    context: context,
+                    cancelWidget: const Text("取消", style: TextStyle(color: Colors.blue),),
+                    confirmWidget: const Text("确定", style: TextStyle(color: Colors.blue),),
+                  );
+                  setState(() {
+                    area = "${result!.provinceName}/${result!.cityName}/${result!.areaName}";
+                  });
+                },
+              ),
+            ),
+
+            const SizedBox(height: 10,),
+            FSText(
+              controller: addressController,
+              text: "详细地址",
+              maxLines: 4,
+              height: 200,
+              onChanged: (value) {
+                addressController.text = value;
+              },
+            ),
+
+            const SizedBox(height: 40,),
+
+            // 修改
+            FSButton(
+              buttonColor: Colors.red,
+              buttonTitle: "修改",
+              tapEvent: () async {
+                bool isLogin = await UserServices.getUserState();
+                if (isLogin) {
+                  // 默认修改成功
+                  Navigator.pop(context);
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/address/address_list_page.dart
+++ lib/address/address_list_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_strong/services/events_bus.dart';
+import 'package:flutter_strong/services/screen_adaper.dart';
+import 'package:flutter_strong/services/user_services.dart';
 
 class AddressListPage extends StatefulWidget {
   const AddressListPage({Key? key}) : super(key: key);
@@ -8,8 +12,193 @@ class AddressListPage extends StatefulWidget {
 }
 
 class _AddressListPageState extends State<AddressListPage> {
+  // 请求列表数据
+  List addressList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _getAddressList();
+
+    eventBus.on<AddressEvent>().listen((event) {
+      print(event);
+      _getAddressList();
+    });
+  }
+
+  _getAddressList() async {
+    // 请求接口
+    var response = [];
+    setState(() {
+      addressList = response;
+    });
+  }
+
+  // 修改默认收货地址
+  _changeDefaultAddress(value) async {
+    // 改成存本地
+    bool isLogin = await UserServices.getUserState();
+    if (isLogin) {
+      // 持久化本地
+
+      // 退出
+      Navigator.pop(context);
+    } else {
+      // 弹出登陆界面
+    }
+  }
+
+  // 删除框
+  _delAddress(value) async {
+    // 本地
+    bool isLogin = await UserServices.getUserState();
+    if (isLogin) {
+      // 删掉本地的某条记录
+
+      // 重新刷新列表
+      _getAddressList();
+    } else {
+      // 弹出登陆界面
+    }
+  }
+
+  _showDelAlertDialog(value) async {
+    await showDialog(
+        // 表示点击灰色背景的时候是否消失弹框
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("提示信息"),
+            content: const Text("您确定要删除吗?"),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("取消"),
+              ),
+              TextButton(
+                child: const Text("确定"),
+                onPressed: () async {
+                  _delAddress(value);
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  // 监听页面销毁
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    eventBus.fire(DefaultAddressEvent("修改收货地址成功..."));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    ScreenAdaper.init(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("收货地址列表"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          // 地址列表
+          ListView.builder(
+              itemCount: addressList.length,
+              itemBuilder: (buildContext, index) {
+                return Column(
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ListTile(
+                      leading: addressList[index]["default_address"] == 1
+                          ? const Icon(
+                              Icons.check,
+                              color: Colors.red,
+                            )
+                          : null,
+                      title: InkWell(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("${addressList[index]["name"]} ${addressList[index]["phone"]}"),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text("${addressList[index]["address"]}"),
+                          ],
+                        ),
+                        onTap: () {
+                          // 修改默认地址
+                          _changeDefaultAddress(addressList[index]["_id"]);
+                        },
+                        onLongPress: () {
+                          // 删除收货地址
+                          _showDelAlertDialog(addressList[index]["_id"]);
+                        },
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(
+                          Icons.edit,
+                          color: Colors.red,
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/addressEdit", arguments: {
+                            "id": addressList[index]["_id"],
+                            "name": addressList[index]["name"],
+                            "phone": addressList[index]["phone"],
+                            "address": addressList[index]["address"],
+                          });
+                        },
+                      ),
+                    ),
+                    const Divider(
+                      height: 20,
+                    ),
+                  ],
+                );
+              }),
+
+          // 底部按钮
+          Positioned(
+            bottom: 0,
+            width: MediaQuery.of(context).size.width,
+            height: ScreenAdaper.height(88),
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              width: MediaQuery.of(context).size.width,
+              height: ScreenAdaper.height(88),
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                border:Border(
+                  top: BorderSide(width: 1, color: Colors.black54),
+                )
+              ),
+              child: InkWell(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Icon(Icons.add, color: Colors.white),
+                    Text("增加收货地址", style: TextStyle(color: Colors.white),),
+                  ],
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, "/addressAdd");
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
