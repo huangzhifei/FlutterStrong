67de0ff177f43e7030bb9cd53b427b12e092fd49
AC_huang <huangzhifei2009@126.com>
2022-07-29 15:37:55 +0800

    完善路由

--- lib/routers/router.dart
+++ lib/routers/router.dart
@@ -1,6 +1,15 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_strong/address/address_add_page.dart';
+import 'package:flutter_strong/address/address_edit_page.dart';
+import 'package:flutter_strong/address/address_list_page.dart';
 import 'package:flutter_strong/cart/cart_page.dart';
 import 'package:flutter_strong/login/login.dart';
+import 'package:flutter_strong/product/check_out_page.dart';
+import 'package:flutter_strong/product/order_info_page.dart';
+import 'package:flutter_strong/product/order_page.dart';
+import 'package:flutter_strong/product/pay_page.dart';
 import 'package:flutter_strong/product/product_content_page.dart';
 import 'package:flutter_strong/product/product_list.dart';
 import 'package:flutter_strong/search/search_page.dart';
@@ -14,6 +23,13 @@ final routes = {
   '/cart': (context) => const CartPage(),
   '/productList': (context, {arguments}) => ProductListPage(arguments: arguments), // 产品列表
   '/productContent': (context, {arguments}) => ProductContentPage(arguments: arguments),// 产品详情
+  '/checkOut': (context) => const CheckOutPage(), // 结算
+  '/addressAdd': (context) => const AddressAddPage(), // 收货地址添加
+  '/addressEdit': (context, {arguments}) => AddressEditPage(arguments: arguments), // 编辑地址
+  '/addressList': (context) => const AddressListPage(), // 收货地址列表
+  '/pay': (context) => const PayPage(), // 支付
+  '/order': (context) => const OrderPage(), // 定单
+  '/orderInfo': (context) => const OrderInfoPage(), // 定单详情
 };
 
 Route<dynamic>? onGenerateRoute(RouteSettings settings) {
